b1693311234aa04733c9a62939cef16d751944b4
volovar <mike@volovar.com>
2016-11-06 12:18:23 -0800

    Completed milestones 1-4

---
+++ Tumblr/HomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HomeViewController.swift
+//  Tumblr
+//
+//  Created by Michael Volovar on 11/6/16.
+//  Copyright © 2016 Michael Volovar. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
---
+++ Tumblr/TabBarViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TabBarViewController.swift
+//  Tumblr
+//
+//  Created by Michael Volovar on 11/6/16.
+//  Copyright © 2016 Michael Volovar. All rights reserved.
+//
+
+import UIKit
+
+class TabBarViewController: UIViewController {
+    @IBOutlet weak var contentView: UIView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
