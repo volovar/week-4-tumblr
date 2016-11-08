322a7f899687ef37919544e596a34e2b2cc70148
volovar <mike@volovar.com>
2016-11-07 21:41:35 -0800

    updated springiness of compose icons

--- Tumblr/ComposeViewController.swift
+++ Tumblr/ComposeViewController.swift
@@ -33,79 +33,77 @@ class ComposeViewController: UIViewController {
     
     override func viewDidAppear(_ animated: Bool) {
         // animate icons into final position
-        // TODO there has to be a better way to do this
-        UIView.animate(withDuration: 0.3, animations: {
+        // TODO there has to be a better way to do this, some sort of recursion?
+        UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 1, options: [.curveEaseInOut], animations: {
             self.photoButton.transform = CGAffineTransform.identity
         })
         
         delay(0.08, closure: {
-            UIView.animate(withDuration: 0.3, animations: {
+            UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 1, options: [.curveEaseInOut], animations: {
                 self.quoteButton.transform = CGAffineTransform.identity
             })
         })
         
         delay(0.16, closure: {
-            UIView.animate(withDuration: 0.3, animations: {
+            UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 1, options: [.curveEaseInOut], animations: {
                 self.chatButton.transform = CGAffineTransform.identity
             })
         })
         
         
         delay(0.24, closure: {
-            UIView.animate(withDuration: 0.3, animations: {
+            UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 1, options: [.curveEaseInOut], animations: {
                 self.textButton.transform = CGAffineTransform.identity
             })
         })
         
         
         delay(0.32, closure: {
-            UIView.animate(withDuration: 0.3, animations: {
+            UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 1, options: [.curveEaseInOut], animations: {
                 self.linkButton.transform = CGAffineTransform.identity
             })
         })
         
-        
         delay(0.4, closure: {
-            UIView.animate(withDuration: 0.3, animations: {
+            UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 1, options: [.curveEaseInOut], animations: {
                 self.videoButton.transform = CGAffineTransform.identity
             })
         })
     }
 
     @IBAction func didPressDismiss(_ sender: UIButton) {
-        UIView.animate(withDuration: 0.3, animations: {
+        UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 1, options: [.curveEaseInOut], animations: {
             self.videoButton.transform = CGAffineTransform(translationX: 0, y: self.view.frame.height - self.videoButton.frame.origin.y)
         })
         
         delay(0.04, closure: {
-            UIView.animate(withDuration: 0.3, animations: {
+            UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 1, options: [.curveEaseInOut], animations: {
                 self.linkButton.transform = CGAffineTransform(translationX: 0, y: self.view.frame.height - self.linkButton.frame.origin.y)
             })
         })
         
         delay(0.08, closure: {
-            UIView.animate(withDuration: 0.3, animations: {
+            UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 1, options: [.curveEaseInOut], animations: {
                 self.textButton.transform = CGAffineTransform(translationX: 0, y: self.view.frame.height - self.textButton.frame.origin.y)
             })
         })
         
         
         delay(0.12, closure: {
-            UIView.animate(withDuration: 0.3, animations: {
+            UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 1, options: [.curveEaseInOut], animations: {
                 self.chatButton.transform = CGAffineTransform(translationX: 0, y: self.view.frame.height - self.chatButton.frame.origin.y)
             })
         })
         
         
         delay(0.16, closure: {
-            UIView.animate(withDuration: 0.3, animations: {
+            UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 1, options: [.curveEaseInOut], animations: {
                 self.quoteButton.transform = CGAffineTransform(translationX: 0, y: self.view.frame.height - self.quoteButton.frame.origin.y)
             })
         })
         
-        
         delay(0.2, closure: {
-            UIView.animate(withDuration: 0.3, animations: {
+            UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 1, options: [.curveEaseInOut], animations: {
                 self.photoButton.transform = CGAffineTransform(translationX: 0, y: self.view.frame.height - self.photoButton.frame.origin.y)
             })
             
